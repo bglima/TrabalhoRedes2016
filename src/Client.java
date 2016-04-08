240097994eb6971ca7b9874dd82d8eb8e702c680
Bruno Lima <bg.lima@hotmail.com>
2016-04-07 23:05:37 -0300

    Testing messages sent from client to server

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /*
  * Class that serve as client
  */
 public class Client {
-
+	Socket socket = null;
+	PrintWriter out = null;
+	BufferedReader in = null;
+	String host = "127.0.0.1";	// also known as localhost
+	public void listenSocket(){
+	   //Create socket connection
+	   try{
+	     socket = new Socket(host, 4321);
+	     out = new PrintWriter(socket.getOutputStream(), true);
+	   } catch (UnknownHostException e) {
+	     System.out.println("Unknown host: "+host);
+	     System.exit(1);
+	   } catch  (IOException e) {
+	     System.out.println("No I/O");
+	     System.exit(1);
+	   }
+	   // Keep sending the same message to the server, every one 1 second
+	   while(true) {
+		   out.println("Hello, server! I am the client.");
+		   try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	   }
+	}
+	
+	public static void main(String [] args) {
+		Client myClient = new Client();
+		myClient.listenSocket();
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+/*
+ * Class that act as server in our application
+ */
 public class Server {
-
+	ServerSocket server = null;
+	Socket client = null;
+	BufferedReader in = null;
+	int port = 4321;
+	public void listenSocket() {
+		// Trying to open a socket in specified port
+		try {
+			server = new ServerSocket(port);
+		} catch (IOException e) {
+			System.out.println("Could not listen from port " + port);
+			System.out.println("Is this already being used?");
+			System.exit(-1);
+		}
+		// Waiting for some client to connect
+		try {
+			System.out.println("Listening to server...");
+			client = server.accept();
+		} catch (IOException e) {
+			System.out.println("Accept failed from "+port+".");
+			System.exit(-1);			
+		}
+		// Create a input file to read client messages
+		try {
+			in = new BufferedReader( new InputStreamReader (client.getInputStream()));
+		} catch (IOException e) {
+			System.out.println("Read failed from client...");
+			System.exit(-1);
+		}	
+		// After first data arrives, do the following
+		while(true){
+			try{
+				Thread.sleep(2000);
+				String line = in.readLine();
+		        System.out.println("Message arrived: "+line);
+		  } catch (IOException e) {
+				System.out.println("Read failed from client...");
+		  } catch (InterruptedException e) {
+				e.printStackTrace();
+		  }
+	   }
+	}
+	public static void main(String[] args) {
+		Server myServer = new Server();
+		myServer.listenSocket();
+	}
 }
+

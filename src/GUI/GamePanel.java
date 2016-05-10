fcd774c7bebcfcf7c511e4e0592f40e4f7170e54
Bruno Lima <bg.lima@hotmail.com>
2016-05-10 20:21:01 -0300

    JPanel that is responsible for load environment and players

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -3,15 +3,23 @@ package GUI;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 import MAP.TileMap;
+import PLAYER.Player;
 
 public class GamePanel extends JPanel implements Runnable {
-	public static final int WIDTH = 400;
-	public static final int HEIGHT = 400;
+	public static final int WIDTH = 416; // 13 tiles * 32 pixels = 416
+	public static final int HEIGHT = 416;
 	
 	private Thread thread;
 	private boolean running;
@@ -23,6 +31,7 @@ public class GamePanel extends JPanel implements Runnable {
 	private int targetTime = 1000/FPS;
 	
 	private TileMap tileMap;
+	private Player player;
 	
 	GamePanel() {
 		super();
@@ -36,6 +45,44 @@ public class GamePanel extends JPanel implements Runnable {
 			thread = new Thread(this);
 			thread.start();
 		}
+		registerKeys();
+	}
+	
+	public void registerKeys() {
+		// Pressed KEY_RIGHT
+		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("RIGHT"), "KEY_RIGHT");
+		this.getActionMap().put("KEY_RIGHT",	new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {	
+				player.moveRight();
+			}
+		});
+		// Pressed KEY_UP
+		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("UP"), "KEY_UP");
+		this.getActionMap().put("KEY_UP",	new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {	
+				player.moveUp();
+			}
+		});
+		// Pressed KEY_LEFT
+		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("LEFT"), "KEY_LEFT");
+		this.getActionMap().put("KEY_LEFT",	new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {	
+				player.moveLeft();
+			}
+		});
+		// Pressed KEY_DOWN
+		this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DOWN"), "KEY_DOWN");
+		this.getActionMap().put("KEY_DOWN",	new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {	
+				player.moveDown();
+			}
+		});
+		this.requestFocus();
+		
+	}
+	
+	public void tell() {
+		System.out.println("Hello");
 	}
 	
 	@Override
@@ -68,15 +115,21 @@ public class GamePanel extends JPanel implements Runnable {
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		g = (Graphics2D) image.getGraphics();
 		
-		tileMap = new TileMap("E:/ThreeJSWorkspace/TrabalhoRedes/src/MAP/testMap.txt", 20);
+		tileMap = new TileMap("E:/ThreeJSWorkspace/TrabalhoRedes/src/MAP/testMap.txt", 32);
+		tileMap.loadTiles("E:/ThreeJSWorkspace/TrabalhoRedes/src/MAP/tileSet.png");
+		player = new Player(tileMap);
+		player.setX(16);
+		player.setY(16);
 	}
 
 	private void update() {
 		tileMap.update();
+		player.update();
 	}
 	
 	private void render() {
 		tileMap.draw(g);
+		player.draw(g);
 	}
 	
 	private void draw() {

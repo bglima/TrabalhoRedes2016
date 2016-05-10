d106f00eed09c77e1139fd21c057f79aab656ff7
Bruno Lima <bg.lima@hotmail.com>
2016-05-10 20:19:24 -0300

    Class that stores one single tile loaded

--- src/MAP/Tile.java
+++ src/MAP/Tile.java
@@ -3,5 +3,13 @@ package MAP;
 import java.awt.image.BufferedImage;
 
 public class Tile {
-	Tile(BufferedImage subImage) {}
+	private BufferedImage image;
+	
+	Tile(BufferedImage subImage) {
+		this.image = subImage;
+	}
+	
+	public BufferedImage getImage() {
+		return image;
+	}
 }
